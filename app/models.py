13ca2a8c1a2a9ac4fca614fe4a0dd41226e164b3
zxqrenwen <zxqrenwen@163.com>
2018-05-29 07:43:11 +0800

    20180529 good morning

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from . import db
+import hashlib
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, AnonymousUserMixin
 from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from datetime import datetime
+from flask import request
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -67,13 +69,22 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
 
     def __init__(self,**kwargs):
         super(User, self).__init__(**kwargs)
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
         if self.email == current_app.config['FLASKY_ADMIN']:
             self.role = Role.query.filter_by(permissions=255).first()
         if self.role is None:
             self.role = Role.query.filter_by(default= True).first()
+
+    def change_email(self, token):
+        self.email = new_email
+        self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        db.session.add(self)
+        return True
 
     def generate_confirmation_token(self, expiration=300):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
@@ -112,6 +123,14 @@ class User(UserMixin, db.Model):
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
+
+    def gravatar(self ,size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url,hash=hash,size=size,default=default,rating=rating)
 
     def __repr__(self):
         return '<User %r>' % self.username
